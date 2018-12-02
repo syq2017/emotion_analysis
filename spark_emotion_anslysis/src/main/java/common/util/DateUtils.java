09aa37d3054137af135597dc16ba871e2b322349
shiyuquan <ishiyuquan@163.com>
2018-12-02 19:30:24 +0800

    dateutils

--- spark_emotion_anslysis/src/main/java/common/util/DateUtils.java
+++ spark_emotion_anslysis/src/main/java/common/util/DateUtils.java
@@ -11,11 +11,7 @@ public class DateUtils {
 
     /**
      * 随机数：休眠时间
-<<<<<<< HEAD
      * @return [5 10]
-=======
-     * @return [15 30]
->>>>>>> 8888fe740d47ddc5c5df5a18fa0f376823c796ae
      */
     public static int getSleepSeconds() {
         Random random = new Random();
@@ -31,14 +27,6 @@ public class DateUtils {
         return random.nextInt(3) + 3;
     }
 
-    /**
-     *随机数：休眠时间-短
-     * @return [5 15]
-     */
-    public static int getShortSleepSeconds() {
-        Random random = new Random();
-        return random.nextInt(10) + 5;
-    }
 
     public static void main(String[] args) {
         System.out.println(getShortSleepSeconds());
